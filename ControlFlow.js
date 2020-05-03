9a3ae1e6ae18c6156aab38efad528131f67baf25
Cristian Alexis Padron Martinez <64509636+cristian237@users.noreply.github.com>
2020-05-03 18:36:30 -0500

    Update ControlFlow.js

--- ControlFlow.js
+++ ControlFlow.js
@@ -49,3 +49,119 @@ do{
     console.log('val of c is ', c);
     c++;
 } while(c<5)
+    
+// if statements //////////////////////////////////////////////////
+const edad = 25;
+
+if(edad > 20){
+    console.log('you are over 23 years old');
+}
+
+const soda = ['coca-cola', 'sprite', 'penafiel', 'Pepsi'];
+
+if(soda.length > 3){
+    console.log("that's a lot of soda");
+}
+
+const password = 'p@ssword';
+
+if(password.length >= 8){
+    console.log('that password is log enough!');
+}
+
+const password2 = 'p@ass';
+
+if(password2.length >= 12){
+    console.log('that password is might strong');
+}
+
+else if(password2.length >= 8){
+    console.log('that password is long enough!');
+} else {
+    console.log('that password is not long lough!');
+}
+
+// logical operators - OR || and AND && /////////////////////////////
+
+const password3 = 'password1234';
+
+if(password3.length >= 12 && password3.includes('@')){
+    console.log('1');
+} else if(password3.length >=8 || password3.includes('a') && password3.length >5){
+    console.log('2');
+} else {
+    console.log('3');
+}
+
+// logical NOT(!) ////////////////////////////////////////////
+
+let user = false;
+
+if(!user){
+    console.log('example negative')
+}
+console.log(!true);
+console.log(!false);
+
+// break and continue ///////////////////////////////////////
+
+const score = [50, 30, 25, 58, 75, 7, 0];
+
+for(let i=0; i<score.length; i++){
+    console.log('tu marcador; ', score[i]);
+
+if(score[i] === 0 ){
+    continue;
+}
+
+console.log('su marcador es: ', score[i]);
+
+if(score[i]=== 100){
+    console.log('your score');
+    break;
+}
+
+}
+
+// switch statements ///////////////////////////////////////
+const grade = 'G';
+
+switch(grade){
+    case 'A':
+        console.log('letra A');
+        break;
+    case 'B':
+        console.log('letra B');
+        break;
+    case 'C':
+        console.log('letra C');
+        break;
+    case 'D':
+        console.log('letra D');
+        break;
+    case 'E':
+        console.log('letra E');
+        break;
+    default:
+        console.log('not a valid grade');
+
+}
+
+// variables and block scope ////////////////////////////////
+
+const age = 30;
+const names5= 'Crsitian';
+
+if(true){
+    const age = 40;
+    const names5 = 'Cristian';
+    console.log('inside 1st code block: ', age, names5);
+
+    if(true){
+        const age = 50;
+        console.log('inside 2st code block: ', age);
+        var test= 'hola';
+    }
+}
+
+console.log('outside code block: ', age, names5, test);
